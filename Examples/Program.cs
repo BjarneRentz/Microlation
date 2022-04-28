e40501fe89552ce8a63a64ac438b3b840fbb1021
Bjarne Rentz <bjarnerentz@live.de>
2022-04-28 12:36:17 +0200

    Added example project to test and show usage

---
+++ Examples/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microlation;
+
+
+var ms1 = new Microservice();
+
+var ms2 = new Microservice()
+{
+    Routes = new List<IRoute>
+    {
+        new Route<int>{Url = "Id"}
+    }
+};
+
+var call = ms1
+    .Call(ms2, new CallOptions<int> { Route = "Id", Policys = new RetryPolicy<int>()})
+    .Validate(value => value != 0);
+
+var result = call.Execute();
+
+
+
+Console.WriteLine(result);
